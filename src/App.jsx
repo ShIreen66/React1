30a6929bf71dc5edcc343100e4157117d8e1d710
ShIreen66 <066shiree@gmail.com>
2024-11-19 18:56:03 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div><h1>{import.meta.env.VITE_PASSWORD}</h1><div className='bg-red-500'>App</div></div>
+    
+    
+  )
+}
+
+export default App
